d41ff8c8a1269ce22b83cbd8cb0a06131e6a5e0a
aungthiha-xenera <aungthiha@xenera.jp>
2021-07-10 04:03:51 +0630

    add carosuel

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,15 +1,50 @@
-import { Layout, Steps, Radio } from "antd";
+import { Layout, Steps, Button } from "antd";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
 import Header from "../../components/Header";
-// import classes from "./styles.module.css";
-
+import Carousel1 from "../../assets/images/carousel1.jpg";
+import Carousel2 from "../../assets/images/carousel2.jpg";
+import Carousel3 from "../../assets/images/carousel3.jpg";
+import Carousel4 from "../../assets/images/carousel4.jpg";
+import classes from "./styles.module.css";
+// import Swiper core and required modules
+import SwiperCore, { Navigation } from "swiper/core";
 const { Content, Footer } = Layout;
 const { Step } = Steps;
 
+// install Swiper modules
+SwiperCore.use([Navigation]);
 const HomePage = () => {
   return (
     <>
       <Layout className="layout">
         <Header />
+        <Content className={classes.container}>
+          <Swiper navigation={true}>
+            <span className={classes.carousel_caption}>
+              <label>Nature Awaits </label>
+              <br />
+              <label className={classes.title}>Explore The World With Us</label>
+            </span>
+            <span className={classes.carousel_button}>
+              <Button type="primary" className={classes.button} size="large">LEARN MORE</Button>
+            </span>
+            <SwiperSlide>
+              <img alt="Carousel" src={Carousel1} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img alt="Carousel" src={Carousel2} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img alt="Carousel" src={Carousel3} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img alt="Carousel" src={Carousel4} />
+            </SwiperSlide>
+          </Swiper>
+        </Content>
         <Content style={{ padding: "0 50px", marginTop: "30px" }}>
           <Steps current={1}>
             <Step title="Finished" description="This is a description." />
@@ -20,16 +55,6 @@ const HomePage = () => {
             />
             <Step title="Waiting" description="This is a description." />
           </Steps>
-          <p>
-          <Radio.Group defaultValue="a" buttonStyle="solid">
-            <Radio.Button value="a">Hangzhou</Radio.Button>
-            <Radio.Button value="b" disabled>
-              Shanghai
-            </Radio.Button>
-            <Radio.Button value="c">Beijing</Radio.Button>
-            <Radio.Button value="d">Chengdu</Radio.Button>
-          </Radio.Group>
-        </p>
         </Content>
         <Footer style={{ textAlign: "center" }}>
           Ant Design ©2018 Created by Ant UED
